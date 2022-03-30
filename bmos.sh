e7fc92172f6e45876237fde534a152572c339608
bob <bob@herzberg.net>
2022-03-30 08:52:42 -0700

    bmos.sh

---
+++ bmos.sh
@@ -0,0 +1,13 @@
+if ! pidof -x aplay > /dev/null
+then
+   /usr/bin/aplay -D default -t raw -r 48000 -c 2 -f S16_LE  /dev/zero &
+fi
+
+touch /home/pi/bmos/parade/parade.txt
+
+/home/pi/bmos/scripts/armsinit.sh
+/home/pi/bmos/scripts/armsupslow.sh &
+omxplayer --layer 10010 -o alsa --no-keys --no-osd /home/pi/bmos/videos/intro.mp4 &
+
+cd /home/pi/bmos/
+/home/pi/bmos/bmos
