323df539237504f0e2f4cf2cde28156fb49362f2
bob <bob@herzberg.net>
2022-03-30 09:07:30 -0700

    Create 10-bmos.sh

---
+++ scripts/10-bmos.sh
@@ -0,0 +1,5 @@
+# launch our autostart apps (if we are on the correct tty)
+if [ "`tty`" = "/dev/tty1" ] && [ "$USER" = "pi" ]; then
+#    bash "/opt/retropie/configs/all/autostart.sh"
+    bash "/home/pi/bmos/scripts/bmos.sh"
+fi
